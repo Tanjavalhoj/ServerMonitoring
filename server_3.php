469f62e7976bc3280c7c3784ab17a594bf4b97d8
Tanja Valhøj <Tanja.valhoj@gmail.com>
2017-11-29 20:48:15 +0100

    Sortering af driftsdata i server 3
    Regex patterne lavet til server 3
    Preg_match_all for kun at få det data der matcher patterne. Smider det
    ind i et array, der hedder matches(...)

--- server_3.php
+++ server_3.php
@@ -22,6 +22,50 @@ $server[0]["pass"] = "";
 //          echo $output_dfih_server_3;
 //          echo "<h2>SSH df -h</h2>";
 //          echo $output_dfh_server_3;
+          
+//-------------------------- Sorting --- dfih ----------------------------------
         
+        $explode = explode("\n", $output_dfh_server_3);
+
+        foreach ($explode as $value) {
+
+            // var_dump($value);  
+        }
+    //SSH data bliver sorteret, og lavet om til et array
+        $pattern = "/([\/a-z-0-9]+)[ ]+([0-9\.(A-Z)]+)+[ ]+([0-9\.(A-Z)]+)+[ ]+([0-9\.(A-Z)]+)+[ ]+([0-9%]+)+[ ]+([\/a-z]+)/";
+
+        if (preg_match_all($pattern, $output_dfh_server_3, $matches_dfih_server_3)) {
         
+            
+        } else {
+            echo "No matches";
+        } 
+        
+//--------------------------Sorting --- dfh ------------------------------------
+
+        $explode_2 = explode("\n", $output_dfh_server_3);
+
+        foreach ($explode_2 as $value_2) {
+
+            // var_dump($value_2);  
+        }
+
+        if (preg_match_all($pattern, $output_dfh_server_3, $matches_dfh)) {
+            //echo 'Matches found: ' . count($matches_dfh) . ' !';
+            //print_r($matches_dfh[0]);
+        } else {
+            echo "No matches";
+        }
+        
+//---------------------------- Db Connection -----------------------------------
+        
+        $servername = "54.154.31.209";
+        $username = "tav";
+        $password = "U88lPRF4R6VjyTfk";
+        $dbnamee = "tav";
+
+        $conn = new mysqli($servername, $username, $password, $dbnamee);
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }        
 ?>
